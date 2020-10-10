5a758fd118d49a59442d0950a0c15d269f3dacd0
wowry <dev.wowry@gmail.com>
2020-10-11 08:17:39 +0900

    Add test

---
+++ test/test.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include "../KissUnescaper.h"
+
+using namespace std;
+
+FILE *openFile(string path, string mode)
+{
+    FILE *fp;
+
+    if ((fp = fopen(path.c_str(), mode.c_str())) == NULL)
+    {
+        cerr << "Error: Failed to open the input file" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return fp;
+}
+
+int main()
+{
+    KissUnescaper unescaper;
+
+    /**
+     * Unescape a binary file
+     */
+    FILE *fp = openFile("test.log", "rb"); // raw file
+
+    unsigned char *data;
+    unsigned data_size;
+
+    data_size = unescaper.unescape(&data, fp);
+
+    // print unescaped data
+    for (int i = 0; i < data_size; i++)
+    {
+        cout << uppercase << hex << int(data[i]) << " ";
+    }
+    cout << endl
+         << endl;
+
+    /**
+     * Unescape an array of some packets
+     */
+    unsigned char packets[] = {0xC0, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0xC0}; // raw packets
+
+    unsigned char *data2;
+    unsigned data_size2;
+
+    data_size2 = unescaper.unescape(&data2, packets, sizeof(packets));
+
+    // print unescaped data
+    for (int i = 0; i < data_size2; i++)
+    {
+        cout << uppercase << hex << int(data2[i]) << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
